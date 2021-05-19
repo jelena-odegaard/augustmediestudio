ebe8b8b43965498113681fbd6df7789654899b6c
jelena-odegaard <57878322+jelena-odegaard@users.noreply.github.com>
2021-05-19 23:23:09 +0200

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,5 +2,3 @@ export const BASE_URL = "https://augustmediestudio.no/wp-json/";
 
 export const TOKEN_PATH = "jwt-auth/v1/token";
 
-//google maps key
-export const KEY = "AIzaSyC8QfJFZNIvYXQVJW1mv0g02stG89Bl30E";
